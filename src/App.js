7352508a13b152ad2a4659c097051033994936cd
Daniele Buccheri <daniele.buccheri@gmail.com>
2023-01-22 19:26:26 -0800

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { uidData } from "./uidData";
+ 
+ 
+ 
+ 
+const Component = () => {
+    const searchParams = new URLSearchParams(document.location.search)
+    const par_uid = searchParams.get('IUD')
+    const getUID = uidData.filter (ID => ID.UID === par_uid )
+ 
+    return (
+        <div>
+        <div id="header">
+        <img src="kplogo.png" width="200px" />
+        </div>
+        <h1 id="UID">
+            Unique Identifier Database
+        </h1>
+       <div > {getUID.map(data => (
+       <div>
+       <div id="data" class="parent">
+ 
+       
+        <div class="child">
+             <span class="label">UID:</span> {data.UID}<br />
+             <span class="label">Project:</span>  {data.Project}<br />
+             <span class="label">Letter ID:</span>  {data.LetterID}<br />
+             <span class="label">Region:</span>  {data.Region}<br />
+             <span class="label">Release Date:</span>  {data.release_date}<br />
+             <span class="label">Revision:</span>  {data.revision}<br />
+             <span class="label">Test Case:</span>  {data.testgroup}<br />
+             <span class="label">Owner Group:</span>  {data.ownergroup}<br />
+             <span class="label">Client:</span>      <a href="mailto:{data.client}"> {data.client} </a><br />
+             <span class="label">TC Contact:</span>   <a href="mailto:{data.tc}"> {data.tc} </a><br />
+             <span class="label">Notes:</span>   {data.notes}<br />
+        </div>
+       </div>
+       </div>
+      
+      ))}</div>
+        </div>
+    )
 }
 
-export default App;
+export default Component;
---
+++ src/uidData.js
@@ -0,0 +1,42 @@
+export const uidData = [
+  {
+    UID: "001",
+    Project: "KPMC",
+    LetterID: "MACL5502",
+    Region: "NCR",
+    release_date: "7-29-2022",
+    revision: "1.2",
+    testgroup: "test001",
+    ownergroup: "DMSS",
+    client: "kpmc.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:"Lorem Ipsum is simply dummya adasd a dasd a dasd asd assd  adsds as asd sa"
+      },
+  {
+    UID: "002",
+    Project: "METRS",
+    LetterID: "0.28",
+    Region: "SCR",
+    release_date: "1-2-2022",
+    revision: "3.0",
+    testgroup: "test003",
+    ownergroup: "DMSS",
+    client: "metrs.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:" Lorem Ipsum has been the industry's standard dummy"
+  },
+  {
+    UID: "003",
+    Project: "PSP",
+    LetterID: "PSP3242",
+    Region: "NCR",
+    release_date: "3-9-2022",
+    revision: "2.0",
+    testgroup: "test005",
+    ownergroup: "DMSS",
+    client: "psp.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:"Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  
+  }
+];
